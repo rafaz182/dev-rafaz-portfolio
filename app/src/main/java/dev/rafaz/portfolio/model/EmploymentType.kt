54667074fcdff5b2080560f98b87f983371fdff8
rafaz <raha148@hotmail.com>
2025-03-04 19:09:46 -0300

    corrigindo build após atualizar IDE

---
+++ app/src/main/java/dev/rafaz/portfolio/model/EmploymentType.kt
@@ -0,0 +1,5 @@
+package dev.rafaz.portfolio.model
+
+enum class EmploymentType {
+    FULL_TIME, INTERNSHIP
+}
---
+++ app/src/main/java/dev/rafaz/portfolio/model/LocationType.kt
@@ -0,0 +1,5 @@
+package dev.rafaz.portfolio.model
+
+enum class LocationType {
+    HYBRID, ON_SITE, REMOTE
+}
